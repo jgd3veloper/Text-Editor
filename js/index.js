789b8cc9e7f045c4b3071687a66dd8e68bdb5c1a
Joseph Giancola <73565558+jgd3veloper@users.noreply.github.com>
2020-11-14 15:39:29 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$('#bold').on('click', function() {
+   document.execCommand('bold', false, null);
+});
+
+$('#italic').on('click', function() {
+   document.execCommand('italic', false, null);
+});
+
+$('#underline').on('click', function() {
+   document.execCommand('underline', false, null);
+});
+
+$('#align-left').on('click', function() {
+   document.execCommand('justifyLeft', false, null);
+});
+
+$('#align-center').on('click', function() {
+   document.execCommand('justifyCenter', false, null);
+});
+
+$('#align-right').on('click', function() {
+   document.execCommand('justifyRight', false, null);
+});
+
+$('#list-ul').on('click', function() {
+   document.execCommand('insertUnorderedList', false, null);
+});
+
+$('#list-ol').on('click', function() {
+   document.execCommand('insertOrderedList', false, null);
+});
+
+$('#fonts').on('change', function() {
+   var font = $(this).val();
+   document.execCommand('fontName', false, font);
+});
+
+$('#size').on('change', function() {
+   var size = $(this).val();
+   $('.editor').css('fontSize', size + 'px');
+});
+
+$('#color').spectrum({
+   color: '#000',
+   showPalette: true,
+   showInput: true,
+   showInitial: true,
+   showInput: true,
+   preferredFormat: "hex",
+   showButtons: false,
+   change: function(color) {
+      color = color.toHexString();
+      document.execCommand('foreColor', false, color);
+   }
+});
+
+$('.editor').perfectScrollbar();
